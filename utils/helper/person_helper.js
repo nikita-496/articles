0606a53f649f8efe54a369218104d842d78ac76c
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-07-14 07:18:53 +0500

    utilities update: addition helper, logger, middleware of error handler

---
+++ utils/helper/person_helper.js
@@ -0,0 +1,36 @@
+const { Repository } = require('../../db/classes/Repository');
+const Person = require('../../db/model/Person');
+const logger = require('../logger')
+
+// Проверить наличие на дублирования логина регистрируемго пользователя с существующими в бд
+/*const checkLogin = (registeredLogin) => {
+  // создать класс (как Repository), который будет искать в бд
+  // сущности(пользователей, блоги и т.д.)
+  // по определенному запросу
+  // 1. создать объект на основе класса, описанного выше
+  // 2. использовать метод объекта для поиска логина
+  // в таблице person
+  // 3. логин есть ?
+  // 4. вызвать ошибку - 409
+  // 5. иначе выйти из программы
+};*/
+
+const createNewPerson = (registrationData, res) => {
+  const columns = Object.keys(registrationData).join(',');
+  const person = new Person(registrationData);
+
+  logger.info("Регистрируемый пользователь:", person)
+  Repository.save('person', columns, person).then((savedUser) => {
+    res.status(201).json(savedUser);
+    return savedUser;
+  });
+};
+
+const getAllPersons = (res) => {
+  Repository.getAll('person', res)
+}
+
+module.exports = {
+  createNewPerson,
+  getAllPersons
+};
---
+++ utils/middleware/error_handler.js
@@ -0,0 +1,12 @@
+const logger = require('../logger');
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message);
+
+  if (error.name === 'Conflict') {
+    return res.status(409);
+  }
+  next(error)
+};
+
+module.exports = errorHandler;
