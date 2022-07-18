09687c2601746f74c0daa395203502f02f280d35
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-07-18 16:45:46 +0500

    test for token assignment process for the registered person

---
+++ tests/token.test.js
@@ -0,0 +1,26 @@
+const supertest = require('supertest');
+const app = require('../app');
+const helper = require('./test_person_helper');
+const logger = require('../utils/logger');
+const api = supertest(app);
+
+describe('a tests for the login process', () => {
+  beforeEach(async () => {
+    await helper.initializeDb();
+  });
+  test('when logging in, the registered person must receive an cookie jwt', async () => {
+    const person = {
+      login: 'Alex',
+      password: 'Alex',
+    };
+
+    const res = await api
+      .post('/api/v1/login')
+      .send(person)
+      .expect('Content-Type', /application\/json/);
+
+    expect(res.headers['set-cookie'][0]).toMatch(/jwt=/);
+    expect(res.body.accessToken).not.toBeNull()
+    expect(res.status).toEqual(200);
+  });
+});
