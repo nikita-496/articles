e7ca0f71a780dd6cd794b360afaa1287fd4bff1d
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-07-20 16:33:33 +0500

    port setting

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 const server = http.createServer(app)
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+
+const PORT = config.PORT || 3001;
+
+server.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
 })
 
 
