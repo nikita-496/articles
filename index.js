34ead554c03123a16a9918a9fb71aecfe87ec759
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-07-20 15:50:18 +0500

    transferring application processing logic

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const express = require('express')
-const http = require('http');
-require('dotenv').config();
+const http = require('http')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
+const server = http.createServer(app)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
 
-app.get('/', (request, response) => {
-  response.end('hello world!')
-})
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
-})
 
