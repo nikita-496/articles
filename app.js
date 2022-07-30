000dc7ab83bd21f501617d12c06acd6a222db346
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-07-30 21:38:46 +0500

    route on comment added

--- app.js
+++ app.js
@@ -15,12 +15,11 @@ app.use('/api/v1/login', require('./routes/login'));
 app.use('/api/v1/refresh', require('./routes/refresh'));
 app.use('/api/v1/logout', require('./routes/logout'));
 
-
-
 app.use('/api/v1/person', require('./routes/person'));
 app.use('/api/v1/profile', require('./routes/profile'))
 app.use('/api/v1/feed', require('./routes/person_feed'))
 app.use('/api/v1/post', require('./routes/post'));
+app.use('/api/v1/comment', require('./routes/comment'));
 
 app.use('/api/v1/joing/profile', require('./routes/joining/feedForProfile'))
 app.use('/api/v1/joing/person', require('./routes/joining/profileForPerson'))
